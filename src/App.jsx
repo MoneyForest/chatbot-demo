eda7b96bb759dbfa78a2d82e5acbca390fe5efd4
MoneyForest <kanamorishuhei@gmail.com>
2020-06-28 23:49:50 +0900

    fix style

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,14 @@ export default class App extends React.Component {
 
   selectAnswer = (selectedAnswer, nextQuestionId ) => {
     switch(true) {
-      case (nextQuestionId === 'init'):
-        this.displayNextQuestion(nextQuestionId)
+      case(nextQuestionId === 'init'):
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500)
+        break;
+      case(/^https:*/.test(nextQuestionId)):
+        const a = document.createElement('a');
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click();
         break;
       default:
         const chats = this.state.chats;
@@ -32,7 +38,7 @@ export default class App extends React.Component {
           chats: chats
         })
 
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 1000)
         break;
       }
   }
@@ -54,6 +60,13 @@ export default class App extends React.Component {
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId);
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area')
+    if(scrollArea){
+        scrollArea.scrollTop = scrollArea.scrollHeight
+    }
   }
 
   render() {
