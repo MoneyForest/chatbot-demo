13e1fa2f093c7d44e4bcd9a11c792407608b6947
MoneyForest <kanamorishuhei@gmail.com>
2020-06-30 00:38:14 +0900

    add dataset to firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import defaultDataset from './dataset';
 import './assets/styles/style.css';
 import { AnswersList, Chats, FormDialog } from './components/index';
+import { db } from './firebase/index'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,7 +10,7 @@ export default class App extends React.Component {
       answers: [],
       chats: [],
       currentId: 'init',
-      dataset: defaultDataset,
+      dataset: {},
       open: false,
     };
 
@@ -71,9 +71,22 @@ export default class App extends React.Component {
     this.setState({open: false});
   };
 
+  initDataset = (dataset) => {
+    this.setState({dataset: dataset});
+  };
+
   componentDidMount() {
-    const initAnswer = "";
-    this.selectAnswer(initAnswer, this.state.currentId);
+    (async() => {
+      const dataset = this.state.dataset
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          dataset[doc.id] = doc.data()
+        })
+      });
+      this.initDataset(dataset);
+      const initAnswer = "";
+      this.selectAnswer(initAnswer, this.state.currentId);
+    })();
   }
 
   componentDidUpdate() {
